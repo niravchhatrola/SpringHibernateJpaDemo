3c3019907abd85cd3a56798ef8533a4bc55a3cc8
niv214 <nirav.chhatrola@gmail.com>
2020-04-21 19:39:11 +0530

    Project Creation and intial commit

---
+++ src/main/java/com/chhatrola/SpringHibernateJpaDemo/SpringHibernateJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chhatrola.SpringHibernateJpaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chhatrola/SpringHibernateJpaDemo/SpringHibernateJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chhatrola.SpringHibernateJpaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
